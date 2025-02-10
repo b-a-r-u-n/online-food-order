3a1ab2a69417cb70be884af694c06291a5742588
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2025-02-10 22:58:32 +0530

    Refactor database connection options and update frontend API base URL

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,12 +3,7 @@ import { dbName } from "../constant.js";
 
 export const connectDb = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            readPreference: 'primary', // Force using the primary node
-            serverSelectionTimeoutMS: 5000, // Wait 5 seconds before timing out
-          });
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`);
         console.log(`MONGO DB connection successfully ${connectionInstance.connection.host}`);
         // const fetched_data = await mongoose.connection.db.collection("menuItems");
         // const data = await fetched_data.find({}).toArray();
