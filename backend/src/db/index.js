ab7c6726bdc9016231cb33633f9c713c10565420
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2025-02-10 22:37:16 +0530

    Update database connection options and change frontend API base URL

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,7 +3,12 @@ import { dbName } from "../constant.js";
 
 export const connectDb = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            readPreference: 'primary', // Force using the primary node
+            serverSelectionTimeoutMS: 5000, // Wait 5 seconds before timing out
+          });
         console.log(`MONGO DB connection successfully ${connectionInstance.connection.host}`);
         // const fetched_data = await mongoose.connection.db.collection("menuItems");
         // const data = await fetched_data.find({}).toArray();
