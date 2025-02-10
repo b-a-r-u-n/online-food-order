cde77b491283b44bfc7f652b2e78d36eea1424c0
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2025-02-11 00:18:10 +0530

    Update CORS configuration to restrict origin to the frontend URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(cors({
     //     'http://192.168.37.1:5173',
     //     'http://192.168.67.237:5173'
     //     ],
-    origin: "*",
+    origin: "https://online-food-order-frontend-o2g7.onrender.com",
     credentials: true,
     // methods: ["GET", "POST", "PUT", "DELETE"],  // ✅ Allow specific methods
     // allowedHeaders: ["Content-Type", "Authorization"],  // ✅ Allow necessary headers
