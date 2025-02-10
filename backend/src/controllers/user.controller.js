ffca09ace6ee128cc859e803b701e0efd40cd78f
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2025-02-11 00:39:33 +0530

    Set SameSite attribute to 'None' for secure cookies in user login

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -89,6 +89,7 @@ const loginUser = asyncHandler( async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
+        sameSite: 'None',
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
     }
 
